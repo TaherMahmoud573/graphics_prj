444ab056b8d5cdf2c98e79b0cd095605580605f8
Taher Yousef <tahermahmoud718@gmail.com>
2025-12-11 19:16:11 +0200

    Operate function. Gate Completed.

--- Components/AND3.cpp
+++ Components/AND3.cpp
@@ -11,7 +11,7 @@ AND3::AND3(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(3, r_FanOut)
 
 void AND3::Operate()
 {
-	//caclulate the output status as the ANDing of the three input pins
+	//caclulate the output status as the ANDing of the three input pin
 
 	//Add you code here
 	if (GetInputPinStatus(1) == HIGH && GetInputPinStatus(2) == HIGH && GetInputPinStatus(3) == HIGH)
