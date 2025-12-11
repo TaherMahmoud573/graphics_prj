32764573e3752b904cb32cdd546e7ab4f76be7e7
Taher Yousef <tahermahmoud718@gmail.com>
2025-12-11 18:05:51 +0200

    Not finished switch code.

--- Components/Switch.h
+++ Components/Switch.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef _Switch_H
+#define _Switch_H
+
+/*
+  Class AND2
+  -----------
+  represent the 2-input AND gate
+*/
+
+#include "Gate.h"
+
+class Switch :public Gate
+{
+public:
+	Switch(const GraphicsInfo& r_GfxInfo, int r_FanOut);
+	virtual void Operate();	//Calculates the output of the AND gate
+	virtual void Draw(Output* pOut);	//Draws 2-input gate
+
+	virtual int GetOutPinStatus();	//returns status of outputpin if LED, return -1
+	virtual int GetInputPinStatus(int n);	//returns status of Inputpin # n if SWITCH, return -1
+
+	virtual void setInputPinStatus(int n, STATUS s);	//set status of Inputpin # n, to be used by connection class.
+
+
+};
+
+#endif
